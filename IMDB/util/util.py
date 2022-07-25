05aecc7ee0613f79d3622b6d0b10b8c3fefea072
Mayank Gajbhiye <mayank1398@gmail.com>
2022-07-25 15:46:13 +0530

    IMDB: util: added yml function to read it

---
+++ IMDB/util/util.py
@@ -0,0 +1,15 @@
+import yaml
+from IMDB.exception import IMDBException
+import sys
+
+
+def read_yaml_file(file_path: str) -> dict:
+    """
+    Reads a YAML file and returns the contents as a dictionary.
+    file_path: str
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise IMDBException(e, sys) from e
